614436256ebc3ab9bead9af54c4c25f5c0fa19b1
nikhil-sharma00 <ns721764@gmail.com>
2026-09-26 12:30:33 +0530

    revert: BullMQ is skipped as service for the time being , adding up BullMQ at this phase was causing stripe errors and server was getting hung

--- apps/worker/src/queues.ts
+++
@@ -1,5 +0,0 @@
-import redis from './lib/redis'
-import { createReminderQueue, createNoShowQueue } from '@perch/shared'
-
-export const reminderQueue = createReminderQueue(redis)
-export const noShowQueue = createNoShowQueue(redis)
